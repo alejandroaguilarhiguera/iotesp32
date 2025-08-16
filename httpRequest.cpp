97587412cb8851f3ad56122d7d6860fea73f2174
Alejandro Aguilar Higuera <alejandro.aguilar.higuera@gmail.com>
2025-08-15 17:28:30 -0700

    change GET method to POST

--- httpRequest.cpp
+++ httpRequest.cpp
@@ -8,18 +8,17 @@ void sendValidationCode(String code) {
   // Solo intenta la petición si el Wi-Fi está conectado
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    const String webHook = HTTP_VALIDATION_CODE;
-    // Construye la URL final
-    String url = webHook + "?code=" + code;
-    
+    const String url = HTTP_VALIDATION_CODE;
+
     Serial.print("Enviando petición a: ");
     Serial.println(url);
 
     // Inicia la petición
     http.begin(url);
-    
-    // Envía la petición GET
-    int httpCode = http.GET();
+    String jsonPayload = "{\"code\":\"" + code + "\"}";
+
+    // Envía la petición POST
+    int httpCode = http.POST(jsonPayload);
 
     if (httpCode > 0) {
       Serial.printf("Petición enviada, código de respuesta: %d\n", httpCode);
